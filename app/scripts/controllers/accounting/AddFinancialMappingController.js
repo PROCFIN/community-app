63f1b248d8204c6d7616d8705287402d86acce6a
Alex Litvinenko <alex@faceit-team.com>
2015-09-24 20:38:13 +0300

    Updated underscore to latest version, firxed Adding Financial Activity Mapping

--- app/scripts/controllers/accounting/AddFinancialMappingController.js
+++ app/scripts/controllers/accounting/AddFinancialMappingController.js
@@ -1,34 +1,45 @@
-(function (module) {
-    mifosX.controllers = _.extend(module, {
-        AddFinancialMappingController: function (scope, resourceFactory, location) {
-            scope.formData = {};
+(function(module) {
+  mifosX.controllers = _.extend(module, {
+    AddFinancialMappingController: function(scope, resourceFactory, location) {
+      scope.formData = {};
+      var financialActivityIds = {};
 
-            resourceFactory.officeToGLAccountMappingResource.get({mappingId:'template'}, function (data) {
-                scope.formData.financialActivityId = 100;
-                scope.glAccountOptions = data.glAccountOptions;
-                scope.financialActivityOptions = data.financialActivityOptions;
-                scope.accountOptions = scope.glAccountOptions.assetAccountOptions;
-            });
+      resourceFactory.officeToGLAccountMappingResource.get({mappingId: 'template'}, function(data) {
+        scope.formData.financialActivityId = 100;
+        scope.glAccountOptions = data.glAccountOptions;
+        scope.financialActivityOptions = data.financialActivityOptions;
+        financialActivityIds = _.indexBy(data.financialActivityOptions, 'id');
+        scope.accountOptions = scope.glAccountOptions.assetAccountOptions;
+      });
 
-            scope.updateActivityOptions = function(activityId){
-                if(activityId === 100){
-                    scope.accountOptions = scope.glAccountOptions.assetAccountOptions;
-                }else if(activityId === 200){
-                    scope.accountOptions = scope.glAccountOptions.liabilityAccountOptions;
-                }else if(activityId === 300){
-                    scope.accountOptions = scope.glAccountOptions.equityAccountOptions;
-                }
-            };
-
-            scope.submit = function () {
-                resourceFactory.officeToGLAccountMappingResource.create(this.formData, function (data) {
-                    location.path('/viewfinancialactivitymapping/' + data.resourceId);
-                });
-            };
-
+      scope.updateActivityOptions = function(financialActivityId) {
+        if (financialActivityIds[financialActivityId]) {
+          scope.accountOptions = getOptionsFormAccountType(financialActivityIds[financialActivityId].mappedGLAccountType);
+        } else {
+          scope.accountOptions = [];
         }
-    });
-    mifosX.ng.application.controller('AddFinancialMappingController', ['$scope', 'ResourceFactory', '$location', mifosX.controllers.AddFinancialMappingController]).run(function ($log) {
-        $log.info("AddFinancialMappingController initialized");
-    });
+      };
+
+      function getOptionsFormAccountType(mappedGLAccountType) {
+        if (mappedGLAccountType === 'ASSET') {
+          return scope.glAccountOptions.assetAccountOptions;
+        } else if (mappedGLAccountType === 'LIABILITY') {
+          return scope.glAccountOptions.liabilityAccountOptions;
+        } else if (mappedGLAccountType === 'EQUITY') {
+          return scope.glAccountOptions.equityAccountOptions;
+        }
+        return [];
+      }
+
+      scope.submit = function() {
+        resourceFactory.officeToGLAccountMappingResource.create(this.formData, function(data) {
+          location.path('/viewfinancialactivitymapping/' + data.resourceId);
+        });
+      };
+
+    }
+  });
+  mifosX.ng.application.controller('AddFinancialMappingController', ['$scope', 'ResourceFactory', '$location', mifosX.controllers.AddFinancialMappingController]).run(function($log) {
+    $log.info("AddFinancialMappingController initialized");
+  });
 }(mifosX.controllers || {}));
--- app/scripts/controllers/accounting/EditFinancialActivityMappingController.js
+++ app/scripts/controllers/accounting/EditFinancialActivityMappingController.js
@@ -1,35 +1,46 @@
-(function (module) {
-    mifosX.controllers = _.extend(module, {
-        EditFinancialActivityMappingController: function (scope, resourceFactory, location,routeParams) {
-            scope.formData = {};
-            scope.accountOptions = [];
-            resourceFactory.officeToGLAccountMappingResource.withTemplate({mappingId: routeParams.mappingId},function (data) {
-                scope.mapping = data;
-                scope.glAccountOptions = data.glAccountOptions;
-                scope.formData.financialActivityId = data.financialActivityData.id;
-                scope.formData.glAccountId = data.glAccountData.id;
-                scope.financialActivityOptions = data.financialActivityOptions;
-                scope.updateActivityOptions(data.financialActivityData.mappedGLAccountType);
-            });
+(function(module) {
+  mifosX.controllers = _.extend(module, {
+    EditFinancialActivityMappingController: function(scope, resourceFactory, location, routeParams) {
+      scope.formData = {};
+      scope.accountOptions = [];
+      var financialActivityIds = {};
+      resourceFactory.officeToGLAccountMappingResource.withTemplate({mappingId: routeParams.mappingId}, function(data) {
+        scope.mapping = data;
+        scope.glAccountOptions = data.glAccountOptions;
+        scope.formData.financialActivityId = data.financialActivityData.id;
+        scope.formData.glAccountId = data.glAccountData.id;
+        scope.financialActivityOptions = data.financialActivityOptions;
+        financialActivityIds = _.indexBy(data.financialActivityOptions, 'id');
+        scope.updateActivityOptions(data.financialActivityData.id);
+      });
 
-            scope.updateActivityOptions = function(mappedGLAccountType){
-                if(mappedGLAccountType === 'ASSET'){
-                    scope.accountOptions = scope.glAccountOptions.assetAccountOptions;
-                }else if(mappedGLAccountType === 'LIABILITY'){
-                    scope.accountOptions = scope.glAccountOptions.liabilityAccountOptions;
-                }else if(mappedGLAccountType === 'EQUITY'){
-                    scope.accountOptions = scope.glAccountOptions.equityAccountOptions;
-                }
-            };
-
-            scope.submit = function () {
-                resourceFactory.officeToGLAccountMappingResource.update({mappingId: routeParams.mappingId},this.formData, function (data) {
-                    location.path('/viewfinancialactivitymapping/' + data.resourceId);
-                });
-            };
+      scope.updateActivityOptions = function(financialActivityId) {
+        if (financialActivityIds[financialActivityId]) {
+          scope.accountOptions = getOptionsFormAccountType(financialActivityIds[financialActivityId].mappedGLAccountType);
+        } else {
+          scope.accountOptions = [];
         }
-    });
-    mifosX.ng.application.controller('EditFinancialActivityMappingController', ['$scope', 'ResourceFactory', '$location','$routeParams', mifosX.controllers.EditFinancialActivityMappingController]).run(function ($log) {
-        $log.info("EditFinancialActivityMappingController initialized");
-    });
+      };
+
+      function getOptionsFormAccountType(mappedGLAccountType) {
+        if (mappedGLAccountType === 'ASSET') {
+          return scope.glAccountOptions.assetAccountOptions;
+        } else if (mappedGLAccountType === 'LIABILITY') {
+          return scope.glAccountOptions.liabilityAccountOptions;
+        } else if (mappedGLAccountType === 'EQUITY') {
+          return scope.glAccountOptions.equityAccountOptions;
+        }
+        return [];
+      }
+
+      scope.submit = function() {
+        resourceFactory.officeToGLAccountMappingResource.update({mappingId: routeParams.mappingId}, this.formData, function(data) {
+          location.path('/viewfinancialactivitymapping/' + data.resourceId);
+        });
+      };
+    }
+  });
+  mifosX.ng.application.controller('EditFinancialActivityMappingController', ['$scope', 'ResourceFactory', '$location', '$routeParams', mifosX.controllers.EditFinancialActivityMappingController]).run(function($log) {
+    $log.info("EditFinancialActivityMappingController initialized");
+  });
 }(mifosX.controllers || {}));
